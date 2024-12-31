38641911067414bb67194767136521bc3545c662
Joshi Priyanka <101914750+priyankajoshiii@users.noreply.github.com>
2024-12-31 11:46:34 +0530

    Delete java scripts directory

--- java scripts/RetrofitAPI.java
+++
@@ -1,9 +0,0 @@
-package com.example.bankbot;
-
-import retrofit2.http.GET;
-import retrofit2.http.Url;
-import retrofit2.Call;
-public interface RetrofitAPI {
-    @GET Call<MessageModel> getMessage(@Url String url);
-
-}
